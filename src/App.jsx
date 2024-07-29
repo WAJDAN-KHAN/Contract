2da7de93bdd81e1d8dcea009e3b330efd67b7974
ahsanch007 <cahsan562@gmail.com>
2024-07-29 12:48:47 +0500

    done changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Main } from './components/Home/Main';
 import { Footer } from './components/Home/Footer';
+import { Token } from './components/Token/Token';
+import ItemsContainer from './components/Details/ItemsContainer';
+import { MainPage } from './components/MainPage/MainPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/home" element={<Main />} />
+        <Route path="/token" element={<Token />} />
+        <Route path="/details" element={<ItemsContainer />} />
       </Routes>
       <Footer />
     </Router>
